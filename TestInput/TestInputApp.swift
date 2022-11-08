8226d1262f205c973e24d561ee31f2077b4c4869
chang8128 <chang8128@sina.cn>
2022-11-08 09:59:47 +0800

    Initial Commit

---
+++ TestInput/TestInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestInputApp.swift
+//  TestInput
+//
+//  Created by 常志平 on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TestInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
